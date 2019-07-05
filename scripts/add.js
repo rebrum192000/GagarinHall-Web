62b0b379f7f48dcb5e3235cf1c0559838134773b
Konstantin <rebrum19200@gmail.com>
2019-07-05 12:58:00 +0300

    Add files via upload

--- scripts/add.js
+++ scripts/add.js
@@ -2,6 +2,27 @@ var add1 = document.querySelector(".dop__service2-btn");
 var add2 = document.querySelector(".dop__service3-btn");
 var check1 = document.querySelector(".tech");
 var check2 = document.querySelector(".photo");
+var addr1 = document.querySelector(".rooms__btn");
+var addr2 = document.querySelector(".rooms2__btn");
+var addr3 = document.querySelector(".rooms3__btn");
+var addr4 = document.querySelector(".rooms4__btn");
+var addr5 = document.querySelector(".rooms5__btn");
+var checkr1 = document.querySelector(".G");
+var checkr2 = document.querySelector(".BC");
+var checkr3 = document.querySelector(".D");
+var checkr4 = document.querySelector(".S");
+var checkr5 = document.querySelector(".A");
+var adds1 = document.querySelector(".sl");
+var adds2 = document.querySelector(".sl2");
+var adds3 = document.querySelector(".sl3");
+var adds4 = document.querySelector(".sl4");
+var adds5 = document.querySelector(".sl5");
+var checks1 = document.querySelector(".g");
+var checks2 = document.querySelector(".bc");
+var checks3 = document.querySelector(".d");
+var checks4 = document.querySelector(".s");
+var checks5 = document.querySelector(".a");
+
 
 add1.onclick = function(){
 	check1.setAttribute("checked","checked");
@@ -9,5 +30,45 @@ add1.onclick = function(){
 }
 add2.onclick = function(){
 	check2.setAttribute("checked","checked");
+	this.checked = true;
+}
+addr1.onclick = function(){
+	checkr1.setAttribute("checked","checked");
+	this.checked = true;
+}
+addr2.onclick = function(){
+	checkr2.setAttribute("checked","checked");
+	this.checked = true;
+}
+addr3.onclick = function(){
+	checkr3.setAttribute("checked","checked");
+	this.checked = true;
+}
+addr4.onclick = function(){
+	checkr4.setAttribute("checked","checked");
+	this.checked = true;
+}
+addr5.onclick = function(){
+	checkr5.setAttribute("checked","checked");
+	this.checked = true;
+}
+adds1.onclick = function(){
+	checks1.setAttribute("checked","checked");
+	this.checked = true;
+}
+adds2.onclick = function(){
+	checks2.setAttribute("checked","checked");
+	this.checked = true;
+}
+adds3.onclick = function(){
+	checks3.setAttribute("checked","checked");
+	this.checked = true;
+}
+adds4.onclick = function(){
+	checks4.setAttribute("checked","checked");
+	this.checked = true;
+}
+adds5.onclick = function(){
+	checks5.setAttribute("checked","checked");
 	this.checked = true;
 }
---
+++ scripts/menu-click.js
@@ -0,0 +1,6 @@
+var menuList = document.querySelector('.nav__list');
+var menuBlock = document.querySelector('.header__nav');
+
+menu.onclick= function() {
+	menuBlock.style.display = 'none';
+}
